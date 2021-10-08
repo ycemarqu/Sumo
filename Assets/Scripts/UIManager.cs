6609072f5466386b96adb3132c26cd14234ed9dc
Muhammet Yüce <yce.marqu@gmail.com>
2021-10-08 18:27:12 +0300

    UI commit
    Ui elements added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,6 +17,7 @@ public class UIManager : MonoBehaviour
 
     private static FoodManager instance = null;
     
+    
     // Game Instance Singleton
     public static FoodManager Instance
     {
@@ -28,7 +29,23 @@ public class UIManager : MonoBehaviour
 
     #endregion
 
-    public SerializableDictionary<UIStates, GameObject> _UIStates;
-    
-    
+    private GameStates _currentState;
+    private GameObject UItoActivate;
+
+    public SerializableDictionary<GameStates, GameObject> _StateCorrespondingUI;
+
+    private void Awake()
+    {
+        GameplayManager.ChangeState += GameplayManagerOnChangeState;
+    }
+
+    private void GameplayManagerOnChangeState(GameStates state)
+    {
+        // Disable Previous UI
+        if(UItoActivate != null)
+            UItoActivate.SetActive(false);
+        // Activate new UI
+        UItoActivate = _StateCorrespondingUI[state];
+        UItoActivate.SetActive(true);
+    }
 }
