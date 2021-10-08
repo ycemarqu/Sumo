1676915097bb439ce2a5dfffb4a6d0a598a0a0b6
Muhammet Yüce <yce.marqu@gmail.com>
2021-10-08 16:19:35 +0300

    Stable Commit Before UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIStates
+{
+    Starting,
+    Playing,
+    End
+    
+}
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static FoodManager instance = null;
+    
+    // Game Instance Singleton
+    public static FoodManager Instance
+    {
+        get
+        { 
+            return instance; 
+        }
+    }
+
+    #endregion
+
+    public SerializableDictionary<UIStates, GameObject> _UIStates;
+    
+    
+}
