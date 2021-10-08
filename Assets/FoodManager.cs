a480eb2191723ffe0be8adf54ec64a985789384b
Muhammet Yüce <yce.marqu@gmail.com>
2021-10-08 14:37:40 +0300

    FoodManager added

--- Assets/FoodManager.cs
+++ Assets/FoodManager.cs
@@ -8,9 +8,11 @@ using Random = UnityEngine.Random;
 
 public class FoodManager : MonoBehaviour
 {
-    public int startingFoodCount;
-    public int maxFoodCount;
+    [Header("Food Counts")]
+    [Tooltip("Food count that is initialized at beginning of level")]public int startingFoodCount;
+    [Tooltip("Maximum Count of Foods that can exist on scene")]public int maxFoodCount;
 
+    [Header("Platform")]
     public GameObject platform;
 
     
@@ -23,61 +25,61 @@ public class FoodManager : MonoBehaviour
     void Start()
     {
         _platformCollider = platform.GetComponent<Collider>();
-        Debug.Log($"Platform Collider bounds {_platformCollider.bounds}");
         
         // Responsibility 1: FoodSpawn
         SetStartingFoods();
-        SpawnFoodWithInterval();
+        InvokeRepeating("RepeatingSpawnFood", 2.0f, spawnInterval);
     }
+    
 
-    private void SpawnFoodWithInterval()
-    {
-        // _platformCollider.bounds.center
-    }
-
-    private void SetStartingFoods()
+    public void SetStartingFoods()
     {
         for (int i = 0; i < startingFoodCount; i++)
         {
-            var obj = ObjectPool._pool.Dequeue();
-
-            var spawnLocation = FindSpawnLocation();
-            obj.transform.position = spawnLocation;
-            obj.SetActive(true);
-            IncrementFood();
+            SpawnFood();
         }
     }
 
-    private Vector3 FindSpawnLocation()
+    public void RepeatingSpawnFood()
+    {
+        if (_currentFoodCount < maxFoodCount)
+            SpawnFood();
+    }
+
+    public void SpawnFood()
+    {
+        //TODO enqueue when it's picked up by players so queue is never empty
+        var obj = ObjectPool._pool.Dequeue();
+        var spawnLocation = FindSpawnLocation();
+        obj.transform.position = spawnLocation;
+        obj.SetActive(true);
+        IncrementFood();
+    }
+
+    public Vector3 FindSpawnLocation()
     {
         var center = _platformCollider.bounds.center;
         var extents = _platformCollider.bounds.extents;
-        Vector3 loc = new Vector3(Random.Range(center.x, center.x + extents.x),
+        Vector3 loc = new Vector3(Random.Range(center.x - extents.x, center.x + extents.x),
             platform.transform.position.y + 0.5f,
-            Random.Range(center.z, center.z + extents.z));
+            Random.Range(center.z - extents.z, center.z + extents.z));
             
-            NavMeshHit hit;
-            Vector3 finalPosition = Vector3.zero;
-            if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
-                finalPosition = hit.position;            
-            }
-            return finalPosition;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(loc, out hit, 3f, 1)) {
+            finalPosition = hit.position;            
+        }
+        return finalPosition;
+        // return loc;
     }
 
     public void DecrementFood()
     {
-        
+        _currentFoodCount--;
     }
 
     public void IncrementFood()
     {
         _currentFoodCount++;
     }
-
-    // private void OnDrawGizmosSelected()
-    // {
-    //     Gizmos.color = Color.red;
-    //     if(_platformCollider != null)
-    //         Gizmos.DrawSphere(_platformCollider.bounds.center,10f);
-    // }
 }
